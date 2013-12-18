42dd0676628f9b1f62a508f6c818a884380f8584
huolg <1244002@qq.com>
2013-12-18 11:40:54 +0800

    guo

--- app/controllers/system_log_controller.rb
+++ app/controllers/system_log_controller.rb
@@ -3,7 +3,7 @@ module SystemLog
   class SystemLogController < ActionController::Base
     # 默认每页显示20条记录
     PER_PAGE = 20
-
+    include SystemLogHelper
     def index
       @logs = SystemLog.paginate(:page => params[:page],
         :per_page => PER_PAGE,
