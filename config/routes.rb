fdf5eb0be66cae77de77aeb294e77492a1d94136
huolg <1244002@qq.com>
2013-12-18 11:06:40 +0800

    guo

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@
 #
 
 #unless Rails.env == 'production'
-  Rails::Engine.routes.draw do
+  ::Engine.routes.draw do
     match 'system_log' => 'system_log#index'
     match 'system_log/clear' => 'system_log#clear'
     root :to => 'system_log#index'
