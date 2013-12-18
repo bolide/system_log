ae9340abf62289b349c48311f867a96a0454f2ff
huolg <1244002@qq.com>
2013-12-18 13:16:38 +0800

    guo

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ SystemLog::Engine.routes.draw do
   #Rails.application.routes.draw do
   match 'system_log' => 'system_log#index'
   match 'system_log/clear' => 'system_log#clear'
-  root :to => 'system_log#index'
+#  root :to => 'system_log#index'
   #end
 end
