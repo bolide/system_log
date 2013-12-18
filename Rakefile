7d75f790e6f779ee678c89fadb0c8bef8333e36f
huolg <1244002@qq.com>
2013-12-18 11:27:04 +0800

    guo

--- Rakefile
+++ Rakefile
@@ -20,16 +20,5 @@ RDoc::Task.new(:rdoc) do |rdoc|
   rdoc.rdoc_files.include('lib/**/*.rb')
 end
 
-# 在此设置Route，可以避免在每个应用程序中设置
 load 'rails/tasks/engine.rake'
 Bundler::GemHelper.install_tasks
-#class ActionController::Routing::RouteSet
-#  alias draw_without_log draw
-#  def draw_with_log
-#    draw_without_log do |map|
-#      map.resources :system_log,:collection => { :clear => :get, :change_level => :get }
-#      yield map
-#    end
-#  end
-#  alias draw draw_with_log
-#end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,7 @@
-## 在此设置Route，可以避免在每个应用程序中设置
-#class ActionController::Routing::RouteSet
-#  alias old_draw draw
-#
-#  def draw
-#    old_draw do |map|
-#      map.system_log 'system_log', :controller=>'system_log'
-#      map.connect 'system_log/clear', :controller=>'system_log', :action => 'clear'
-#      yield map
-#    end
-#  end
-#
-#end
-#
-#
-
-#unless Rails.env == 'production'
-#  SystemLog::Engine.routes.draw do
-Rails.application.routes.draw do
-    match 'system_log' => 'system_log#index'
-    match 'system_log/clear' => 'system_log#clear'
-#    root :to => 'system_log#index'
+SystemLog::Engine.routes.draw do
+  #Rails.application.routes.draw do
+  match 'system_log' => 'system_log#index'
+  match 'system_log/clear' => 'system_log#clear'
+  root :to => 'system_log#index'
+  #end
 end
-#  end
-#end
